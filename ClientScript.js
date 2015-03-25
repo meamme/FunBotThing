594fdf007beb095bb7b33d6d45d807f3ea5de868
Matheus Avellar <Tetheu98@users.noreply.github.com>
2015-03-24 21:31:26 -0300

    Woop

--- ClientScript.js
+++ ClientScript.js
@@ -434,7 +434,7 @@ var bcs = {
 		bcs.displayLvl();
 		$("#playlist-panel").on('click',function(){setTimeout(function(){$("#dialog-preview").draggable({ containment: "#app", scroll: false });},500)});
 		favoritism();
-		if ($("#bcsVersionCheck").css('z-index') != 144){
+		if ($("#bcsVersionCheck").css('left') != 144){
 			bcs.addChat("<br>Looks like Github is being slow again. The CSS for this version of BCS has not been updated yet.","#C42E3B",true,true);
 			bcs.addChat("<br>You may use BCS, but some things might show up weird","#eee");
 		}
@@ -2779,5 +2779,5 @@ function commandStuff(data){
 			break;
 	};
 }
-bcs.turnOn();//Github pls
+bcs.turnOn();
 }
